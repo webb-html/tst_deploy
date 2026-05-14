17c9677f1b4bd78e9c6fb49e699cb039af55a50f
Arseniy.shrk <arseniy.shrk@gmail.com>
2026-05-14 20:45:55 +0700

    2

--- app.py
+++ app.py
@@ -33,8 +33,6 @@ login_manager.init_app(app)
 
 APP_URL = 'http://127.0.0.1:5000'
 
-@app.route('/')
-@app.route('/index')
 def index(): # главная страница
     if current_user.is_authenticated:
         notes_json = get(f'{APP_URL}/api/notes').json()
@@ -231,7 +229,12 @@ def download_note(user_name, note_id): # скачивание файла
                          download_name=current_note['title'] + '.txt',
                          as_attachment=True)
 
+@app.route('/')
+@app.route('/index')
+def main():
+    return 'hello world'
+
 if __name__ == '__main__':
-    db_session.global_init("db/data.db")
+    #db_session.global_init("db/data.db")
     print(f'server is running {APP_URL}')
     serve(app, port=os.environ.get("PORT", 5000), host='0.0.0.0')
